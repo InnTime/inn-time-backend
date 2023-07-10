793b66e8de05f15eb3a5f74e5f14100660c84018
Poseidondon <superzarub@rambler.ru>
2023-07-10 14:32:25 +0300

    User tests implemented

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,5 +4,62 @@ def test_login_fail(client):
         "password": "123"
     }
     response = client.post('/login', json=data)
-
     assert response.status_code == 401
+
+
+def test_register_fail_1(client):
+    data = {
+        "email": "real@email.com"
+    }
+    response = client.post('/register', json=data)
+    assert response.status_code == 400
+
+
+def test_register_success(client):
+    data = {
+        "email": "real@email.com",
+        "password": "123"
+    }
+    response = client.post('/register', json=data)
+    assert response.status_code == 200
+
+    data = {
+        "email": "real@email.com",
+        "password": "123"
+    }
+    response = client.post('/register', json=data)
+    assert response.status_code == 400
+
+
+def test_login_success(client):
+    data = {
+        "email": "real@email.com",
+        "password": "123"
+    }
+    response = client.post('/login', json=data)
+    assert response.status_code == 200
+
+
+def test_update_password(client):
+    # get token
+    data = {
+        "email": "real@email.com",
+        "password": "123"
+    }
+    response = client.post('/login', json=data)
+    assert response.status_code == 200
+
+    # update password
+    headers = {"Authorization": f"Bearer {response.json['accessToken']}"}
+    data = {
+        'password': 'new_password'
+    }
+    assert response.status_code == 200
+
+    # check if password correct
+    data = {
+        "email": "real@email.com",
+        "password": "new_password"
+    }
+    response = client.post('/login', json=data)
+    assert response.status_code == 200
